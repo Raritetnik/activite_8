f07d83b6d8fcd868e2e8a30bdcc12fa9034cef16
Raritetnik <101665571+Raritetnik@users.noreply.github.com>
2022-04-14 15:52:36 -0400

    Messages de JS

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 window.onload=function(){
-    document.querySelector('#btn').addEventListener('click', () => {
-        console.log('Hello, Console!');
+    document.querySelector('#btn__message').addEventListener('click', () => {
+        console.log('Salut, Console!');
         fun_test();
+    });
+
+    document.querySelector('#btn__warning').addEventListener('click', () => {
+        console.warn('Faite attention!');
+    });
+
+    document.querySelector('#btn__error').addEventListener('click', () => {
+    console.error(`Une erreur est survenu!`);
     });
   }
 
